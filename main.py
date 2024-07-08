4f79d206e58d1754d43e840554b5b2e9903e172f
SanntAlv <saalvarez@fi.uba.ar>
2024-07-08 19:09:36 -0300

    Creacion de las rutas login, jugar y seleccionar dificultad

--- main.py
+++ main.py
@@ -3,19 +3,33 @@ from models import db, Usuario, Puntuacion
 
 app = Flask(__name__)
 port = 5000
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/mibasededatos'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/mibasededatos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 
 @app.route('/')
 def hello_world():
-    return "Hello world"
+    return "sdfsfasdasd"
 
-@app.route('/data/<section>')
-def data(section):
-    return section
+@app.route('/usuarios/<id_usuarios>')
+def data(id_usuarios):
+    print('Usuario: ',id_usuarios)
+    return id_usuarios
 
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    nombre = request.form.get('nombre')
+    usuario = Usuario.query.filter_by(nombre=nombre).first()
+    if not usuario:
+        usuario = Usuario(nombre=nombre)
+        db.session.add(usuario)
+        db.session.commit()
+    session['usuario_id'] = usuario.id
+    return redirect(url_for('select_dificultad'))
 
 
 if __name__ == '__main__':
